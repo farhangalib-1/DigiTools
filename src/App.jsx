17fac501e00820349e6df1687df0d84729fc7ed0
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-03-30 01:27:38 +0600

    Rating section is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import HeroBg from './components/HeroBg'
 import Navbar from './components/Navbar'
+import Stat from './components/Stat'
 const App = () => {
   return (
     <>
     <Navbar />
     <HeroBg />
+    <Stat />
     </>
   )
 }
---
+++ src/components/Stat.jsx
@@ -0,0 +1,22 @@
+const Stat = () => {
+  return (
+    <div className="mt-9 bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white">
+        <div className=" w-10/12 mx-auto flex justify-between py-15 px-10">
+            <div className="">
+                <h1 className="text-[60px] font-bold text text-center">50K+</h1>
+                <p className="text-center">Active Users</p>
+            </div>
+            <div className="border-l-2 border-r-2 px-40">
+                <h1 className="text-[60px] font-bold text">200+</h1>
+                <p className="text-center">Premium Tools</p>
+            </div>
+            <div className="">
+                <h1 className="text-[60px] font-bold text">4.9</h1>
+                <p className="text-center">Rating</p>
+            </div>
+            </div>
+    </div>
+  )
+}
+
+export default Stat
